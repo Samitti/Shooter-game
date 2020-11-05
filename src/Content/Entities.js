aad1735ef2e47ece60ab34dd3df7ef40538bb25d
Samitti <samuelteweldebrhan@gmail.com>
2020-11-05 17:09:03 +0300

    add apawn enemies and the ability that player and enemies to shoot

--- src/Content/Entities.js
+++ src/Content/Entities.js
@@ -16,6 +16,10 @@ class Player extends Entity {
     super(scene, x, y, key, "Player");
     this.setData("speed", 200);
     this.play("sprPlayer");
+    
+    this.setData("isShooting", false);
+    this.setData("timerShootDelay", 10);
+    this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
   }
 
   moveUp() {
@@ -39,8 +43,120 @@ class Player extends Entity {
 
     this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
     this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    if (this.getData("isShooting")) {
+      if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+        this.setData("timerShootTick", this.getData("timerShootTick") + 1); 
+      }
+      else { 
+        var laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+      
+        // this.scene.sfx.laser.play();
+        this.setData("timerShootTick", 0);
+      }
+    }
   }
   
 }
 
-export { Player, Entity };
+class PlayerLaser extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprLaserPlayer");
+    this.body.velocity.y = -200;
+  }
+}
+
+class ChaserDragon extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprEnemy1", "ChaserDragon");
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.states = {
+      MOVE_DOWN: "MOVE_DOWN",
+      CHASE: "CHASE"
+    };
+    this.state = this.states.MOVE_DOWN;
+  }
+
+  update() {
+    if (!this.getData("isDead") && this.scene.player) {
+      if (Phaser.Math.Distance.Between(
+        this.x,
+        this.y,
+        this.scene.player.x,
+        this.scene.player.y
+      ) < 320) {
+
+        this.state = this.states.CHASE;
+      }
+
+      if (this.state == this.states.CHASE) {
+        var dx = this.scene.player.x - this.x;
+        var dy = this.scene.player.y - this.y;
+
+        var angle = Math.atan2(dy, dx);
+
+        var speed = 100;
+        this.body.setVelocity(
+          Math.cos(angle) * speed,
+          Math.sin(angle) * speed
+        );
+      }
+    }
+
+    
+    if (this.x < this.scene.player.x) {
+      this.angle -= 2;
+    }
+    else {
+      this.angle += 2;
+    } 
+  }
+}
+
+class FighterDragon extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprEnemy0", "FighterDragon");
+    this.body.velocity.y = Phaser.Math.Between(50, 100);
+    this.shootTimer = this.scene.time.addEvent({
+      delay: 1000,
+      callback: function() {
+        var laser = new EnemyLaser(
+          this.scene,
+          this.x,
+          this.y
+        );
+        laser.setScale(this.scaleX);
+        this.scene.enemyLasers.add(laser);
+      },
+      callbackScope: this,
+      loop: true
+    });
+    this.play("sprEnemy0");
+  }
+
+  onDestroy() {
+    if (this.shootTimer !== undefined) {
+      if (this.shootTimer) {
+        this.shootTimer.remove(false);
+      }
+    }
+  }
+}
+
+class SpyDragon extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprEnemy2", "SpyDragon");
+    this.body.velocity.y = Phaser.Math.Between(20, 100);
+    this.play("sprEnemy2");
+  }
+}
+
+class EnemyLaser extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprLaserEnemy0");
+    this.body.velocity.y = 200;
+  }
+}
+
+export { Player, Entity, ChaserDragon, FighterDragon, SpyDragon, EnemyLaser, PlayerLaser };
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,5 @@
 import 'phaser';
-import { Player } from '../Content/Entities';
+import { Player, ChaserDragon, FighterDragon, SpyDragon, EnemyLaser, PlayerLaser } from '../Content/Entities';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
@@ -12,6 +12,21 @@ export default class GameScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 32
     });
+
+    this.load.spritesheet("sprEnemy0", "assets/sprEnemy0.png", {
+      frameWidth: 93,
+      frameHeight: 65
+    });
+
+    this.load.image("sprEnemy1", "assets/sprEnemy1.png");
+    this.load.spritesheet("sprEnemy2", "assets/sprEnemy2.png", {
+      frameWidth: 93,
+      frameHeight: 65
+    });
+
+    this.load.image("sprLaserEnemy0", "assets/sprLaserEnemy0.png");
+    this.load.image("sprLaserPlayer", "assets/sprLaserPlayer.png");
+
   }
 
   create () {
@@ -30,11 +45,69 @@ export default class GameScene extends Phaser.Scene {
       "sprPlayer"
     );   
 
+    this.anims.create({
+      key: "sprEnemy0",
+      frames: this.anims.generateFrameNumbers("sprEnemy0"),
+      frameRate: 5,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "sprEnemy2",
+      frames: this.anims.generateFrameNumbers("sprEnemy2"),
+      frameRate: 5,
+      repeat: -1
+    });
+    
+
     this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE); 
+
+    this.enemies = this.add.group();
+    this.enemyLasers = this.add.group();
+    this.playerLasers = this.add.group();
+
+    this.time.addEvent({
+      delay: 2000,
+      callback: function() {
+        var enemy = null;
+
+        if (Phaser.Math.Between(0, 10) >= 3) {
+          enemy = new FighterDragon(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0
+          );
+        }
+        else if (Phaser.Math.Between(0, 10) >= 5) {
+          if (this.getEnemiesByType("ChaserDragon").length < 5) {
+
+            enemy = new ChaserDragon(
+              this,
+              Phaser.Math.Between(0, this.game.config.width),
+              0
+            );
+          }
+        }
+        else {
+          enemy = new SpyDragon(
+            this,
+            Phaser.Math.Between(0, this.game.config.width),
+            0
+          );
+        }
+
+        if (enemy !== null) {
+          enemy.setScale(Phaser.Math.Between(5, 15) * 0.1);
+          this.enemies.add(enemy);
+        }
+      },
+      callbackScope: this,
+      loop: true
+    });
   }
 
   update() {
@@ -53,7 +126,30 @@ export default class GameScene extends Phaser.Scene {
     else if (this.keyD.isDown) {
       this.player.moveRight();
     }
+
+    if (this.keySpace.isDown) {
+      this.player.setData("isShooting", true);
+    }
+    else {
+      this.player.setData("timerShootTick", this.player.getData("timerShootDelay") - 1);
+      this.player.setData("isShooting", false);
+    }
+
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+
+      enemy.update();
+    }
   }
   
-  
+  getEnemiesByType(type) {
+    var arr = [];
+    for (var i = 0; i < this.enemies.getChildren().length; i++) {
+      var enemy = this.enemies.getChildren()[i];
+      if (enemy.getData("type") == type) {
+        arr.push(enemy);
+      }
+    }
+    return arr;
+  }
 };
